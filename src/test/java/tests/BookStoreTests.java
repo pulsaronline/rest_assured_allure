060709b61fb3d9a7e884cc7c5fcb08779662fd64
Pulsaronline <beslite@gmail.com>
2021-05-29 13:37:31 +0300

    Code clean

--- src/test/java/tests/BookStoreTests.java
+++ src/test/java/tests/BookStoreTests.java
@@ -1,7 +1,6 @@
 package tests;
 
 import io.qameta.allure.restassured.AllureRestAssured;
-import io.restassured.response.Response;
 import models.AuthorisationResponse;
 import models.Books;
 import org.junit.jupiter.api.Test;
@@ -13,15 +12,13 @@ import static filters.CustomLogFilter.customLogFilter;
 import static io.restassured.RestAssured.given;
 import static io.restassured.http.ContentType.JSON;
 import static io.restassured.module.jsv.JsonSchemaValidator.matchesJsonSchemaInClasspath;
-import static org.aeonbits.owner.util.Util.asString;
 import static org.hamcrest.CoreMatchers.is;
-import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.greaterThan;
 import static org.hamcrest.Matchers.hasSize;
 
 public class BookStoreTests {
     @Test
-    void noLogsTest(){
+    void noLogsTest() {
         given()
                 .get("https://demoqa.com/BookStore/v1/Books")
                 .then()
@@ -29,7 +26,7 @@ public class BookStoreTests {
     }
 
     @Test
-    void withAllLogsTest(){
+    void withAllLogsTest() {
         given()
                 .log().all()
                 .get("https://demoqa.com/BookStore/v1/Books")
@@ -39,7 +36,7 @@ public class BookStoreTests {
     }
 
     @Test
-    void withSomeLogsTest(){
+    void withSomeLogsTest() {
         given()
                 .log().uri()
                 .log().body()
@@ -50,7 +47,7 @@ public class BookStoreTests {
     }
 
     @Test
-    void withSomePostTest(){
+    void withSomePostTest() {
         given()
                 .contentType(JSON)
                 .body("{ \"userName\": \"alex\", \"password\": \"W1_#zqwerty\" }")
@@ -65,7 +62,7 @@ public class BookStoreTests {
     }
 
     @Test
-    void withAllureListenerTest(){
+    void withAllureListenerTest() {
         Map<String, Object> data = new HashMap<>();
         data.put("userName", "alex");
         data.put("password", "W1_#zqwerty");
@@ -85,7 +82,7 @@ public class BookStoreTests {
     }
 
     @Test
-    void withCustomFilterTest(){
+    void withCustomFilterTest() {
         Map<String, Object> data = new HashMap<>();
         data.put("userName", "alex");
         data.put("password", "W1_#zqwerty");
@@ -105,68 +102,68 @@ public class BookStoreTests {
     }
 
     @Test
-    void withAssertJTest(){
+    void withAssertJTest() {
         Map<String, Object> data = new HashMap<>();
         data.put("userName", "alex");
         data.put("password", "W1_#zqwerty");
         String response =
-        given()
-                .contentType(JSON)
-                .filter(customLogFilter().withCustomTemplates())
-                .body(data)
-                .when()
-                .log().uri()
-                .log().body()
-                .post("https://demoqa.com/Account/v1/GenerateToken")
-                .then()
-                .log().body()
-                .extract().asString();
-        assert(response).contains("\"status\":\"Success\"");
-        assert(response).contains("\"result\":\"User authorized successfully.\"");
+                given()
+                        .contentType(JSON)
+                        .filter(customLogFilter().withCustomTemplates())
+                        .body(data)
+                        .when()
+                        .log().uri()
+                        .log().body()
+                        .post("https://demoqa.com/Account/v1/GenerateToken")
+                        .then()
+                        .log().body()
+                        .extract().asString();
+        assert (response).contains("\"status\":\"Success\"");
+        assert (response).contains("\"result\":\"User authorized successfully.\"");
     }
 
     @Test
-    void withModelTest(){
+    void withModelTest() {
         Map<String, Object> data = new HashMap<>();
         data.put("userName", "alex");
         data.put("password", "W1_#zqwerty");
         AuthorisationResponse response =
-        given()
-                .contentType(JSON)
-                .filter(customLogFilter().withCustomTemplates())
-                .body(data)
-                .when()
-                .log().uri()
-                .log().body()
-                .post("https://demoqa.com/Account/v1/GenerateToken")
-                .then()
-                .log().body()
-                .extract().as(AuthorisationResponse.class);
-        assert(response.getStatus()).contains("Success");
-        assert(response.getResult()).contains("User authorized successfully.");
+                given()
+                        .contentType(JSON)
+                        .filter(customLogFilter().withCustomTemplates())
+                        .body(data)
+                        .when()
+                        .log().uri()
+                        .log().body()
+                        .post("https://demoqa.com/Account/v1/GenerateToken")
+                        .then()
+                        .log().body()
+                        .extract().as(AuthorisationResponse.class);
+        assert (response.getStatus()).contains("Success");
+        assert (response.getResult()).contains("User authorized successfully.");
     }
 
     @Test
-    void booksModelTest(){
+    void booksModelTest() {
         Books books =
+                given()
+                        .log().uri()
+                        .log().body()
+                        .get("https://demoqa.com/BookStore/v1/Books")
+                        .then()
+                        .log().body()
+                        .extract().as(Books.class);
+        //System.out.println(books);
+    }
+
+    @Test
+    void booksJsonSchemaTest() {
         given()
                 .log().uri()
                 .log().body()
                 .get("https://demoqa.com/BookStore/v1/Books")
                 .then()
                 .log().body()
-                .extract().as(Books.class);
-        //System.out.println(books);
-    }
-
-    @Test
-    void booksJsonSchemaTest(){
-                        given()
-                        .log().uri()
-                        .log().body()
-                        .get("https://demoqa.com/BookStore/v1/Books")
-                        .then()
-                        .log().body()
-                        .body(matchesJsonSchemaInClasspath("jsonSchemas/booklist_response.json"));
+                .body(matchesJsonSchemaInClasspath("jsonSchemas/booklist_response.json"));
     }
 }
